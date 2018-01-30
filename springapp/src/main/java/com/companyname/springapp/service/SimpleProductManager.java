94c9edd77d94f310fb2748e9017a009aef3a6476
Julio <Julio@user-PC>
2018-01-30 14:27:19 +0100

    Conexion con base de datos

--- springapp/src/main/java/com/companyname/springapp/service/SimpleProductManager.java
+++ springapp/src/main/java/com/companyname/springapp/service/SimpleProductManager.java
@@ -2,24 +2,39 @@ package com.companyname.springapp.service;
 
 import java.util.List;
 
-import com.companyname.springapp.domain.Product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.companyname.springapp.domain.Product;
+import com.companyname.springapp.repository.ProductDao;
+
+@Component
 public class SimpleProductManager implements ProductManager {
 
     private static final long serialVersionUID = 1L;
+    
+    @Autowired
+    private ProductDao productDao;
+
+    public void setProductDao(ProductDao productDao) {
+        this.productDao = productDao;
+    }
 
     private List<Product> products;
 
     public List<Product> getProducts() {
-        return products; 
+    	return productDao.getProductList();
     }
 
     public void increasePrice(int percentage) {
+    	List<Product> products = productDao.getProductList();
     	if (products != null) {
             for (Product product : products) {
                 double newPrice = product.getPrice().doubleValue() * 
                                     (100 + percentage)/100;
                 product.setPrice(newPrice);
+                productDao.saveProduct(product);
             }
         }   
     }
